1b2bb64b4a5bb1531acc37cb0aae02d8e4784285
AvusalaChetan <avusalachetan@gmail.com>
2025-12-04 11:45:26 +0530

    Fix route ordering for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
-const frontendPath = path.join(__dirname, "../frontend");
-app.use(express.static(frontendPath));
-
 app.use("/auth", require("./router/authRouter"));
 app.use("/note", require("./router/notesRouter"));
 app.use("/", require("./router/profile"));
+
+const frontendPath = path.join(__dirname, "../frontend");
+app.use(express.static(frontendPath));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'html', 'login.html'));
+});
+
 
 app.use((req, res) => {
   res.status(404).json({success: false, message: "404 page"});
@@ -28,4 +33,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({success: false, message: "Something went wrong!"});
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
